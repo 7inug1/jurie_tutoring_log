22010869d1677bc94dec8c62c7cb01a3d179e052
7inug1 <7inug1@gmail.com>
2021-10-14 11:52:28 +0900

    펜션 웹사이트 script navigation 작업

---
+++ 20회차/js/script.js
@@ -0,0 +1,196 @@
+let entireProvinces_sub1 = {
+  title: '전국1',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let entireProvinces_sub2 = {
+  title: '전국2',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let entireProvinces_sub3 = {
+  title: '전국3',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let entireProvinces_sub4 = {
+  title: '전국4',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let entireProvinces_sub5 = {
+  title: '전국5',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+
+
+let gyeonggi_sub1 = {
+  title: '경기1',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gyeonggi_sub2 = {
+  title: '경기2',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gyeonggi_sub3 = {
+  title: '경기3',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gyeonggi_sub4 = {
+  title: '경기4',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gyeonggi_sub5 = {
+  title: '경기5',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+
+
+
+let gangwon_sub1 = {
+  title: '강원1',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gangwon_sub2 = {
+  title: '강원2',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gangwon_sub3 = {
+  title: '강원3',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gangwon_sub4 = {
+  title: '강원4',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gangwon_sub5 = {
+  title: '강원5',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+
+chungcheong_jeolla_sub1 = {
+  title: '충청/전라1',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+chungcheong_jeolla_sub2 = {
+  title: '충청/전라2',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+chungcheong_jeolla_sub3 = {
+  title: '충청/전라3',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+chungcheong_jeolla_sub4 = {
+  title: '충청/전라4',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+chungcheong_jeolla_sub5 = {
+  title: '충청/전라5',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+
+
+let gyeongsang_jeju_sub1 = {
+  title: '경상/제주1',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gyeongsang_jeju_sub2 = {
+  title: '경상/제주2',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gyeongsang_jeju_sub3 = {
+  title: '경상/제주3',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gyeongsang_jeju_sub4 = {
+  title: '경상/제주4',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let gyeongsang_jeju_sub5 = {
+  title: '경상/제주5',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+
+
+let leisure_culture_sub1 = {
+  title: '레져/문화1',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let leisure_culture_sub2 = {
+  title: '레져/문화2',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let leisure_culture_sub3 = {
+  title: '레져/문화3',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let leisure_culture_sub4 = {
+  title: '레져/문화4',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let leisure_culture_sub5 = {
+  title: '레져/문화5',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+
+
+let weekends_veryDay_sub1 = {
+  title: '주말/당일권1',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let weekends_veryDay_sub2 = {
+  title: '주말/당일권2',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let weekends_veryDay_sub3 = {
+  title: '주말/당일권3',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let weekends_veryDay_sub4 = {
+  title: '주말/당일권4',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+let weekends_veryDay_sub5 = {
+  title: '주말/당일권5',
+  list: ["플로라","멀리가는향기","은가비","리버빌","남이섬옹달샘","대추나무사랑걸렸네","게스트하우스","레이크힐","힐스토리","아델하임","모닝"]
+}
+
+
+let entireProvinces = { title: '전국', list: [entireProvinces_sub1, entireProvinces_sub2, entireProvinces_sub3, entireProvinces_sub4, entireProvinces_sub5] }
+let gyeonggi = { title: '경기', list: [gyeonggi_sub1, gyeonggi_sub2, gyeonggi_sub3, gyeonggi_sub4, gyeonggi_sub5] }
+let gangwon = { title: '강원', list: [gangwon_sub1, gangwon_sub2, gangwon_sub3, gangwon_sub4, gangwon_sub5] }
+let chungcheong_jeolla = { title: '충청/전라', list: [chungcheong_jeolla_sub1, chungcheong_jeolla_sub2, chungcheong_jeolla_sub3, chungcheong_jeolla_sub4, chungcheong_jeolla_sub5] }
+let gyeongsang_jeju = { title: '경상/제주', list: [gyeongsang_jeju_sub1, gyeongsang_jeju_sub2, gyeongsang_jeju_sub3, gyeongsang_jeju_sub4, gyeongsang_jeju_sub5] }
+let leisure_culture = { title: '레져/문화', list: [leisure_culture_sub1, leisure_culture_sub2, leisure_culture_sub3, leisure_culture_sub4, leisure_culture_sub5] }
+let weekends_veryDay = { title: '주말/당일권', list: [weekends_veryDay_sub1, weekends_veryDay_sub2, weekends_veryDay_sub3, weekends_veryDay_sub4, weekends_veryDay_sub5] }
+let navigationLists = [entireProvinces, gyeonggi, gangwon, chungcheong_jeolla, gyeongsang_jeju, leisure_culture, weekends_veryDay]
+
+
+let navigationWrapper2 = document.querySelector('.navigation-wrapper2')
+// console.log(provinces)
+// provinces.map((province)=>console.log(province))
+console.log(navigationLists)
+let navigation = document.createElement('ul')
+navigation.className = "navigation"
+let subnavigation = document.createElement('ul')
+subnavigation.className = "subnavigation"
+let navigationStr = ''
+let subnavigationStr = ''
+
+// navigationLists.map((navigationList) => { console.log(navigationList.title) })
+navigationLists.map((navigationList) => {
+  navigationStr += `<li>
+  <button type="button" class="button00">
+    <span class="tail">
+      <span class="body">
+        <span class="number">532</span>
+        ${navigationList.title}</span>
+      </span>
+    </button>
+  </li>`
+})
+
+navigationLists.map((navigationList) => {
+  subnavigationStr += `
+    
+  `
+})
+
+navigation.innerHTML = navigationStr
+navigationWrapper2.appendChild(navigation)
