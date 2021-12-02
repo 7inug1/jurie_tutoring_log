e285d62e16a587cd17e10c05813ed611e956f770
7inug1 <7inug1@gmail.com>
2021-12-02 14:38:47 +0900

    modal 마무리

--- 21회차/modal3회차/script.js
+++ 21회차/modal3회차/script.js
@@ -1,7 +1,7 @@
 let body = document.querySelector('.main-body');
 let modals = document.querySelectorAll('.modal');
 let modalContainer = document.createElement('div');
-modalContainer.className = 'modal-container';
+modalContainer.id = 'modal-container';
 let overflowHidden = 'overflow-hidden';
 let defaultSetting = {
   dim: true,
@@ -17,43 +17,38 @@ let defaultSetting = {
   cancelButton: '취소',
 };
 
-// 화면에 있는 modal 클릭 시
 window.addEventListener('click', (event) => {
   if (event.target.className === 'modal') {
-    // let buttonContainer = event.target.children[0].children[2];
-    // let confirmButton = event.target.children[0].children[2].children[0];
-    // let cancelButton;
-    // console.log(event.target.children[0].children[3]);
     let clonedModal = event.target.children[0].cloneNode(true);
-
     modalObj.clone(clonedModal);
   }
 });
 
 const modalObj = {
-  id: 1,
-  getId: (id) => {
-    // let id = modalObj.id;
-    // let idStr = `id${id}`;
-    // id++;
-    // console.log(id);
-    // return idStr;
-    // let id = 1;
-    function incrementId(id) {
-      this.id = id++;
-      return this.id;
+  assignModalId: (wrapper) => {
+    let length = modalContainer.children.length;
+    let modalId;
+    if (length === 0) {
+      modalId = `modal-0`;
+      wrapper.id = modalId;
+    } else {
+      let lastModalId = Number(
+        modalContainer.childNodes[length - 1].id.split('-')[1]
+      );
+      let newModalId = `modal-${lastModalId + 1}`;
+      wrapper.id = newModalId;
     }
   },
   open: (setting) => {
-    createBodyOverflow();
+    modalObj.createBodyOverflow();
     console.log(!body.contains(modalContainer));
-    createModalContainer(!body.contains(modalContainer));
+    modalObj.createModalContainer(!body.contains(modalContainer));
 
-    let header = getHeader(setting.title);
-    let main = getMain(setting.content);
-    let wrapper = getWrapper();
-    let dim = getDim(wrapper, setting.dim);
-    let wrapper01 = getWrapper01(header, main, wrapper, setting, dim);
+    let header = modalObj.getHeader(setting.title);
+    let main = modalObj.getMain(setting.content);
+    let wrapper = modalObj.getWrapper();
+    let dim = modalObj.getDim(wrapper, setting.dim);
+    let wrapper01 = modalObj.getWrapper01(header, main, wrapper, setting, dim);
 
     wrapper.appendChild(wrapper01);
     wrapper.appendChild(dim);
@@ -63,14 +58,14 @@ const modalObj = {
     modalContainer.removeChild(wrapper);
     wrapper.removeChild(dim);
 
-    removeModalContainer(modalContainer.hasChildNodes());
-    removeBodyOverflow();
+    modalObj.removeModalContainer(modalContainer.hasChildNodes());
+    modalObj.removeBodyOverflow();
   },
   clone: (clonedModal) => {
     console.log(this);
 
-    let wrapper = getWrapper();
-    let dim = getDim(wrapper, true);
+    let wrapper = modalObj.getWrapper();
+    let dim = modalObj.getDim(wrapper, true);
     console.log(clonedModal);
     let closeButton = clonedModal.querySelector('.close-button');
     let buttonContainer = clonedModal.querySelector('.button-container');
@@ -92,144 +87,134 @@ const modalObj = {
       modalObj.close(wrapper, dim);
     });
 
-    createBodyOverflow();
-    createModalContainer(!body.contains(modalContainer));
+    modalObj.createBodyOverflow();
+    modalObj.createModalContainer(!body.contains(modalContainer));
 
     wrapper.appendChild(clonedModal);
     wrapper.appendChild(dim);
     modalContainer.appendChild(wrapper);
   },
-};
+  getHeader: (titleText) => {
+    let header = document.createElement('div');
+    header.className = 'header';
+    let title = document.createElement('h1');
+    title.className = 'title01';
+    title.innerHTML = `${titleText}`;
 
-function getHeader(titleText) {
-  let header = document.createElement('div');
-  header.className = 'header';
-  let title = document.createElement('h1');
-  title.className = 'title01';
-  title.innerHTML = `${titleText}`;
+    header.appendChild(title);
 
-  header.appendChild(title);
+    return header;
+  },
+  getMain: (content) => {
+    let main = document.createElement('div');
+    main.className = 'main01';
+    main.innerHTML = content;
 
-  return header;
-}
+    return main;
+  },
+  createBodyOverflow: () => {
+    if (!body.classList.contains(overflowHidden)) {
+      body.classList.add(overflowHidden);
+    }
+  },
+  removeBodyOverflow: () => {
+    if (
+      body.classList.contains(overflowHidden) &&
+      !(body.lastChild.className === 'modal-container')
+    ) {
+      body.classList.remove(overflowHidden);
+    }
+  },
+  getButtonContainer: (confirmButtonText, cancelButtonText, wrapper, dim) => {
+    let buttonContainer = document.createElement('div');
+    buttonContainer.className = 'button-container';
 
-function getMain(content) {
-  let main = document.createElement('div');
-  main.className = 'main01';
-  main.innerHTML = content;
+    buttonContainer.appendChild(
+      modalObj.getConfirmButton(wrapper, confirmButtonText, dim)
+    );
+    buttonContainer.appendChild(
+      modalObj.getCancelButton(wrapper, cancelButtonText, dim)
+    );
 
-  return main;
-}
+    return buttonContainer;
+  },
+  getConfirmButton: (wrapper, confirmButtonText, dim) => {
+    let confirmButton = document.createElement('button');
+    confirmButton.className = 'button01_head_orange';
+    let confirmButton_tail = document.createElement('span');
+    confirmButton_tail.className = 'button01_tail';
+    let confirmButton_body = document.createElement('span');
+    confirmButton_body.className = 'button01_body';
+    confirmButton_body.innerHTML = `${confirmButtonText}`;
 
-function createBodyOverflow() {
-  if (!body.classList.contains(overflowHidden)) {
-    body.classList.add(overflowHidden);
-  }
-}
+    confirmButton.appendChild(confirmButton_tail);
+    confirmButton_tail.appendChild(confirmButton_body);
 
-function removeBodyOverflow() {
-  if (
-    body.classList.contains(overflowHidden) &&
-    !(body.lastChild.className === 'modal-container')
-  ) {
-    body.classList.remove(overflowHidden);
-  }
-}
-
-function getButtonContainer(confirmButtonText, cancelButtonText, wrapper, dim) {
-  let buttonContainer = document.createElement('div');
-  buttonContainer.className = 'button-container';
-
-  buttonContainer.appendChild(
-    getConfirmButton(wrapper, confirmButtonText, dim)
-  );
-  buttonContainer.appendChild(getCancelButton(wrapper, cancelButtonText, dim));
-
-  return buttonContainer;
-}
-
-function getConfirmButton(wrapper, confirmButtonText, dim) {
-  let confirmButton = document.createElement('button');
-  confirmButton.className = 'button01_head_orange';
-  let confirmButton_tail = document.createElement('span');
-  confirmButton_tail.className = 'button01_tail';
-  let confirmButton_body = document.createElement('span');
-  confirmButton_body.className = 'button01_body';
-  confirmButton_body.innerHTML = `${confirmButtonText}`;
-
-  confirmButton.appendChild(confirmButton_tail);
-  confirmButton_tail.appendChild(confirmButton_body);
-
-  confirmButton.addEventListener('click', () => {
-    modalObj.close(wrapper, dim);
-  });
-
-  return confirmButton;
-}
-
-function getCancelButton(wrapper, cancelButtonText, dim) {
-  let cancelButton = document.createElement('button');
-  cancelButton.className = 'button01_head_white';
-  let cancelButton_tail = document.createElement('span');
-  cancelButton_tail.className = 'button01_tail';
-  let cancelButton_body = document.createElement('span');
-  cancelButton_body.className = 'button01_body';
-  cancelButton_body.innerHTML = `${cancelButtonText}`;
-
-  cancelButton.appendChild(cancelButton_tail);
-  cancelButton_tail.appendChild(cancelButton_body);
-  cancelButton.addEventListener('click', () => {
-    modalObj.close(wrapper, dim);
-  });
-
-  return cancelButton;
-}
-
-function createModalContainer(hasChild) {
-  if (hasChild) {
-    body.appendChild(modalContainer);
-  }
-}
-function removeModalContainer(hasChild) {
-  if (!hasChild) {
-    body.removeChild(modalContainer);
-  }
-}
-
-// modalFrame
-function getWrapper() {
-  let wrapper = document.createElement('div');
-  wrapper.className = 'wrapper';
-
-  return wrapper;
-}
-
-function getWrapper01(header, main, wrapper, setting, dim) {
-  let wrapper01 = document.createElement('div');
-  wrapper01.className = 'wrapper01';
-  wrapper01.appendChild(header);
-  wrapper01.appendChild(main);
-  wrapper01.appendChild(
-    getButtonContainer(
-      setting.confirmButton,
-      setting.cancelButton,
-      wrapper,
-      dim
-    )
-  );
-
-  return wrapper01;
-}
-
-function getDim(wrapper, isClosable) {
-  let dim = document.createElement('div');
-  dim.className = 'dim';
-  if (isClosable) {
-    dim.addEventListener('click', () => {
-      console.log('here');
+    confirmButton.addEventListener('click', () => {
       modalObj.close(wrapper, dim);
     });
-  }
 
-  return dim;
-}
+    return confirmButton;
+  },
+  getCancelButton: (wrapper, cancelButtonText, dim) => {
+    let cancelButton = document.createElement('button');
+    cancelButton.className = 'button01_head_white';
+    let cancelButton_tail = document.createElement('span');
+    cancelButton_tail.className = 'button01_tail';
+    let cancelButton_body = document.createElement('span');
+    cancelButton_body.className = 'button01_body';
+    cancelButton_body.innerHTML = `${cancelButtonText}`;
+
+    cancelButton.appendChild(cancelButton_tail);
+    cancelButton_tail.appendChild(cancelButton_body);
+    cancelButton.addEventListener('click', () => {
+      modalObj.close(wrapper, dim);
+    });
+
+    return cancelButton;
+  },
+  createModalContainer: (hasChild) => {
+    if (hasChild) {
+      body.appendChild(modalContainer);
+    }
+  },
+  removeModalContainer: (hasChild) => {
+    if (!hasChild) {
+      body.removeChild(modalContainer);
+    }
+  },
+  getWrapper: () => {
+    let wrapper = document.createElement('div');
+    wrapper.className = 'wrapper';
+    modalObj.assignModalId(wrapper);
+    return wrapper;
+  },
+  getWrapper01: (header, main, wrapper, setting, dim) => {
+    let wrapper01 = document.createElement('div');
+    wrapper01.className = 'wrapper01';
+    wrapper01.appendChild(header);
+    wrapper01.appendChild(main);
+    wrapper01.appendChild(
+      modalObj.getButtonContainer(
+        setting.confirmButton,
+        setting.cancelButton,
+        wrapper,
+        dim
+      )
+    );
+
+    return wrapper01;
+  },
+  getDim: (wrapper, isClosable) => {
+    let dim = document.createElement('div');
+    dim.className = 'dim';
+    if (isClosable) {
+      dim.addEventListener('click', () => {
+        console.log('here');
+        modalObj.close(wrapper, dim);
+      });
+    }
+
+    return dim;
+  },
+};
